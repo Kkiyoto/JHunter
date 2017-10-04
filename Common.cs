0bc09249102b0a942789f5942bf04a00eab90707
Kkiyoto <kiyoto@dss-i.jp>
2017-10-04 20:57:08 +0900

    Common.csにクラスを書く

---
+++ Common.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Common : MonoBehaviour
+{
+    public enum State
+    {
+        Wait=0,
+        Walk,
+        Dig,
+        Get,
+    }
+}
+
+public class Field :MonoBehaviour
+{
+    GameObject obj;
+    int num;
+
+    public Field(GameObject o)
+    {
+        obj = o;
+    }
+}
---
+++ Stage10.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage10 : MonoBehaviour
+{
+    Common.State state;
+
+	// Use this for initialization
+	void Start ()
+    {
+        state = Common.State.Wait;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        switch (state)
+        {
+            case Common.State.Wait:
+                Waiting();
+                break;
+            case Common.State.Walk:
+                Walking();
+                break;
+            case Common.State.Dig:
+                Digging();
+                break;
+            case Common.State.Get:
+                Getting();
+                break;
+        }
+
+		
+	}
+
+    void Waiting()
+    {
+
+    }
+    void Walking()
+    {
+
+    } 
+    void Digging()
+    {
+
+    }
+    void Getting()
+    {
+
+    }
+}
